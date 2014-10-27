a73891d14fde5e3dab2f28c09e431096dd4e37f2
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-10-27 15:39:51 +0300

    Hide custom fields while project creation

--- lib/redmine_projectsettings_hidecustomfields/patches/app/controllers/projects_controller.rb
+++ lib/redmine_projectsettings_hidecustomfields/patches/app/controllers/projects_controller.rb
@@ -7,6 +7,7 @@ module ProjectSettingsHideCustomFields
 
 			base.class_eval do
 				alias_method_chain :settings, :projectsettings_hidecustomfields
+				alias_method_chain :new,      :projectsettings_hidecustomfields
 			end
 		end
 
@@ -17,6 +18,10 @@ module ProjectSettingsHideCustomFields
 			def settings_with_projectsettings_hidecustomfields
 				settings_without_projectsettings_hidecustomfields
 				@issue_custom_fields = [] if !User.current.allowed_to?(:manage_custom_fields, @project) && !User.current.admin?
+			end
+			def new_with_projectsettings_hidecustomfields
+				new_without_projectsettings_hidecustomfields
+				@issue_custom_fields = [] unless User.current.admin?
 			end
 		end
 	end
